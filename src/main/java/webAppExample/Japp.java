46b199e732eaeaaacc03aa764134d02983117140
user1 <user1@accenture.com>
2018-06-10 12:58:20 +0530

    sop 1 added

--- src/main/java/webAppExample/Japp.java
+++ src/main/java/webAppExample/Japp.java
@@ -6,5 +6,7 @@ public class Japp {
 	public static void main(String[] args) {
 		System.out.println("Hello world .. Git");
 		System.out.println("Hello world .. Accenture");
+		System.out.println("Hello world .. 1");
+		
 	}
 }
