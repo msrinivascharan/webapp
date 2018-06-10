ac441e9d17e6fbe6fcf1ce7d4cbf7bb262c5a968
user1 <user1@HDC2-L-2BD8NN2.dir.svc.accenture.com>
2018-06-10 09:21:59 +0530

    web app changes @ user1

--- src/main/java/webAppExample/Japp.java
+++ src/main/java/webAppExample/Japp.java
@@ -5,11 +5,7 @@ public class Japp {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world .. Git");
-<<<<<<< HEAD
 		System.out.println("Hello world .. Accenture");
-=======
-		
->>>>>>> refs/remotes/origin/master
 	}
 
 }
