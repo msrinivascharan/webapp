b859a15a16c86e42aa646f907d61a9b095dd90eb
user1 <user1@accenture.com>
2018-06-10 13:51:32 +0530

    sop added for hdcb

--- src/main/java/webAppExample/Japp.java
+++ src/main/java/webAppExample/Japp.java
@@ -5,11 +5,6 @@ public class Japp {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world .. Git");
-		System.out.println("Hello world .. Accenture-HDC");
-		System.out.println("Hello world .. 1");
-		System.out.println("Hello world .. 2");
-		System.out.println("Hello world .. 3");
-		
-
+		System.out.println("Hello world .. Accenture-HDCB");
 	}
 }
