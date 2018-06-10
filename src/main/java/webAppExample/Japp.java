98125c3f5aedc552f013e504e48c810c78808979
user1 <user1@accenture.com>
2018-06-10 13:44:37 +0530

    sop added hdc4

--- src/main/java/webAppExample/Japp.java
+++ src/main/java/webAppExample/Japp.java
@@ -5,7 +5,7 @@ public class Japp {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world .. Git");
-		System.out.println("Hello world .. Accenture");
+		System.out.println("Hello world .. Accenture-HDC4");
 		System.out.println("Hello world .. 1");
 		System.out.println("Hello world .. 2");
 		System.out.println("Hello world .. 3");
