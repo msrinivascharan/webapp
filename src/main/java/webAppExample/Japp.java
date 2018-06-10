84bd489e1268daccd708c9221ffa3fb97b9e92ca
user2 <user2@accenture.com>
2018-06-10 23:26:59 +0530

    c2 added

--- src/main/java/webAppExample/Japp.java
+++ src/main/java/webAppExample/Japp.java
@@ -1,7 +1,8 @@
 package webAppExample;
 //this comment added for cherry pick
 public class Japp {
-//c1	
+//c1
+	//c2
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world .. Git");
